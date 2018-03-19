7c2db03a075069ad97bc6a7db6f69d905285fd80
Nemanja Bacic <nbacic@ymail.com>
2018-03-19 16:18:20 +0100

    Added image from google as background.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,18 @@
 <template>
-  <div>
-    <div class='main'>
-    <div class='wicon'>
-      <img :src="forecast.forecastday[0].day.condition.icon" :alt="city">
+<div>
+  <div class="main-app" v-bind:style="{ 'background-image': 'url(https://maps.googleapis.com/maps/api/place/photo?maxwidth=12000&photoreference=' + image + '&key=AIzaSyCYwUml9eACiBtWu_24pVk07h-zzOrJghc)' }">
+    <div class='main' >
+      <div class='wicon'>
+        <img :src="forecast.forecastday[0].day.condition.icon" :alt="city">
+      </div>
+      <h1>{{ city }}</h1>
+      <div class="current-tem">
+        {{current.temp_c + '°C'}} 
+        <p>Feels like: {{ current.feelslike_c + '°C' }}</p>
+        
+      </div>
     </div>
-    <h1>{{ city }}</h1>
-    <div class="current-tem">
-      {{current.temp_c + '°C'}} 
-      <p>Feels like: {{ current.feelslike_c + '°C' }}</p>
-    </div>
+    
   </div>
   <gmap-map
       :options="{scrollwheel: false}"
@@ -18,7 +22,8 @@
         <gmap-marker
         :position="{lat:location.lat, lng:location.lon}"></gmap-marker>
     </gmap-map>
-  </div>
+</div>
+  
 </template>
 
 <script>
@@ -31,11 +36,11 @@ export default {
   data () {
     return {
       city: '',
-      region: '',
-      country: '',
       current: [],
       forecast: [],
-      location: []
+      location: [],
+      placeId: '',
+      image: ''
     }
   },
   mounted() {
@@ -48,13 +53,18 @@ export default {
         this.city = response.data.city
         axios.get(`https://api.apixu.com/v1/forecast.json?key=324b63316a4440e4ad675706181903&q=${response.data.city}`)
           .then(response => {
-            console.log(response)
             this.current = response.data.current
             this.forecast = response.data.forecast
             this.location = response.data.location
-            axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${this.city}&key=AIzaSyCYwUml9eACiBtWu_24pVk07h-zzOrJghc`)
+
+            const proxyurl = "https://cors-anywhere.herokuapp.com/";
+            axios.get( proxyurl + `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${this.city}&key=${apiKey}`)
             .then(res => {
-              this.places = res.data
+              this.placeId = res.data.results[0].place_id
+              axios.post(proxyurl + `https://maps.googleapis.com/maps/api/place/details/json?placeid=${this.placeId}&key=${apiKey}`)
+              .then(response => {
+                this.image = response.data.result.photos[0].photo_reference
+              })
             })
           })
       });
@@ -65,31 +75,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.main{
-  margin: 50px auto;
-  padding:20px;
-  background-color:#424242;
-  opacity:0.7;
-  width:500px;
-  height:200px;
-  color:white;
-  border-radius:50px;
-  position: relative;
-}
-.wicon{
-  width:100px;
-  height:100px;
-  border-radius:100%;
-  position:absolute;
-  top:-55px;
-  box-shadow:0 0 1em white;
-  border-style:solid;
-  color:white;
-}
-.wicon img {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+.main-app {
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
 }
 </style>
